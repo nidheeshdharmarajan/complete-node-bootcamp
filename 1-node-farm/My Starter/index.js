d66d8a580f7d3c62fa32bd9f7d9a1b0bbb315757
Nidheesh Dharmarajan <nidheesh.dharmarajan@trenser.com>
2024-11-09 00:11:56 +0530

    1

--- 1-node-farm/My Starter/index.js
+++ 1-node-farm/My Starter/index.js
@@ -1,8 +1,25 @@
 const fs = require("fs")
+// const { CLIENT_RENEG_LIMIT } = require("tls")
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8")
-const hello = "just hallo"
-console.log(textIn)
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8")
+// const hello = "just hallo"
+// console.log(textIn)
 
-const textOut = `" this is all  we know about avacado:${textIn}. \n Created on ${Date.now()}`
-fs.writeFileSync("./txt/output.txt", textOut)
+// const textOut = `" this is all  we know about avacado:${textIn}. \n Created on ${Date.now()}`
+// fs.writeFileSync("./txt/outputt.txt", textOut)
+
+// console.log(textOut);
+
+fs.readFile('./txt/star5t.txt','utf-8',(err,data1)=>{
+    if(err){return console.log('ayyooo');}
+    fs.readFile(`./txt/${data1}.txt`,'utf8',(err,data2)=>{
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`,'utf8',(err,data3)=>{
+            console.log(data3);
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
+                console.log("writen on the file");
+            })
+        })    
+    })
+})
+console.log('will read the file');
