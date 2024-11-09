4acbd38a478f08bfafaa58ec51aa0679f4a99f14
Nidheesh Dharmarajan <nidheesh.dharmarajan@trenser.com>
2024-11-10 01:47:31 +0530

    server added

--- 1-node-farm/My Starter/index.js
+++ 1-node-farm/My Starter/index.js
@@ -10,16 +10,36 @@ const fs = require("fs")
 
 // console.log(textOut);
 
-fs.readFile('./txt/star5t.txt','utf-8',(err,data1)=>{
-    if(err){return console.log('ayyooo');}
-    fs.readFile(`./txt/${data1}.txt`,'utf8',(err,data2)=>{
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`,'utf8',(err,data3)=>{
-            console.log(data3);
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
-                console.log("writen on the file");
-            })
-        })    
-    })
+// fs.readFile('./txt/star5t.txt','utf-8',(err,data1)=>{
+//     if(err){return console.log('ayyooo');}
+//     fs.readFile(`./txt/${data1}.txt`,'utf8',(err,data2)=>{
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`,'utf8',(err,data3)=>{
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
+//                 console.log("writen on the file");
+//             })
+//         })    
+//     })
+// })
+// console.log('will read the file');
+const http = require('http')
+const url = require('url')
+const server = http.createServer((req, res) => {
+    const pathName=req.url
+    if (pathName ==='/' || pathName ==='/overview') {
+        res.end('This is over view')
+        console.log(req.url);
+    } else if (pathName == '/product') {
+        res.end('This is product')
+        console.log(req.url);
+    } else {
+        res.end('page not found')
+        console.log(req.url);
+    }
+
+
 })
-console.log('will read the file');
+server.listen(8000, '127.0.0.1', () => {
+    console.log('lestening to the the request on port 8000');
+})
