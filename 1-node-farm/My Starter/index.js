8948c7f920ab4035e0a225f2a2558fea946eeee3
Nidheesh Dharmarajan <nidheesh.dharmarajan@trenser.com>
2024-11-10 09:58:27 +0530

    first api

--- 1-node-farm/My Starter/index.js
+++ 1-node-farm/My Starter/index.js
@@ -1,4 +1,4 @@
-const fs = require("fs")
+const fs = require("fs");
 // const { CLIENT_RENEG_LIMIT } = require("tls")
 
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8")
@@ -19,27 +19,44 @@ const fs = require("fs")
 //             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
 //                 console.log("writen on the file");
 //             })
-//         })    
+//         })
 //     })
 // })
 // console.log('will read the file');
-const http = require('http')
-const url = require('url')
+const http = require("http");
+const url = require("url");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf8");
+dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-    const pathName=req.url
-    if (pathName ==='/' || pathName ==='/overview') {
-        res.end('This is over view')
-        console.log(req.url);
-    } else if (pathName == '/product') {
-        res.end('This is product')
-        console.log(req.url);
-    } else {
-        res.end('page not found')
-        console.log(req.url);
-    }
-
-
-})
-server.listen(8000, '127.0.0.1', () => {
-    console.log('lestening to the the request on port 8000');
-})
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is over view");
+    console.log(req.url);
+  } else if (pathName == "/product") {
+    res.end("This is product");
+    console.log(req.url);
+  } else if (pathName === "/api") {
+    // fs.readFile(`${__dirname}/dev-data/data.json`, "utf8", (err, data) => {
+    //   const productData = JSON.parse(data);
+    //   console.log(productData);
+    //   res.writeHead(200, { "content-type": "application/json" });
+    //   res.end(data);
+    // });
+    // res.end("api");
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
+  } else {
+    //     res.end('page not found')
+    //     console.log(req.url);
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page note found</h1>");
+  }
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("lestening to the the request on port 8000");
+});
